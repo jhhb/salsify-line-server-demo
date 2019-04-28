019c90c0b9fdefb563e087f5d1534dda54684a11
James <jhhboyle@gmail.com>
2019-04-28 14:52:00 -0400

    Add file path and line count as config settings * Add debug endpoint of GET /line_count * Handle requesting out-of-range file index with 413 * Update run.sh to set env vars FILE_PATH and FILE_LINE_COUNT

--- lib/controller.rb
+++ lib/controller.rb
@@ -6,6 +6,11 @@ require_relative 'my_model'
 
 module App
   class Controller < Sinatra::Base
+    configure do
+      set :file_path, ENV['FILE_PATH']
+      set :file_length, ENV['FILE_LINE_COUNT'].to_i
+    end
+
     get '/keys' do
       redis.keys.map { |k| { key: k, value: redis.get(k) } }.to_json
     end
@@ -15,7 +20,28 @@ module App
       redis.set(key, "value-for-#{key}")
     end
 
+    # Endpoint for debugging purposes
+    get '/line_count/:count' do
+      settings.file_line_count.to_s
+    end
+
+    get '/lines/:index' do
+      if params[:index].to_i > file_length
+        [
+          413,
+          "You requested a file index past the file length of #{file_length}"
+        ]
+      else
+        'Valid'
+      end
+    end
+
     private
+
+    # @return [Fixnum] number of lines in the file
+    def file_length
+      settings.file_length
+    end
 
     def redis
       @redis ||= Redis.new(host: '127.0.0.1', port: 6379, db: 1)
--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+if [[ $# -eq 0 ]] ; then
+    echo 'Must supply path of the file as first argument'
+    echo 'Usage: sh run.sh <path_to_file>'
+    exit 1
+fi
+
+args=("$@")
+
+export FILE_PATH=${args[0]}
+echo "Running server with FILE_PATH ${FILE_PATH}..."
+export FILE_LINE_COUNT=`cat ${FILE_PATH} | wc -l`
+echo "Calculated FILE_LINE_COUNT of ${FILE_LINE_COUNT} for FILE_PATH ${FILE_PATH}..."
 redis-server &
+echo "Started redis-server..."
 bundle exec passenger start
+echo "Started passenger..."
